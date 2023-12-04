7cde4cdb1d64c4b5f32ad4188d68e267e1158855
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-12-04 18:11:26 +0530

    Implement createNewLecturer database section

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -3,8 +3,6 @@ package lk.ijse.dep11.edupanel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
